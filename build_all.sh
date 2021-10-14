17e701dc4235392f518e78aa2b9b7dc75204b2e2
nevenanikolicc <85739181+nevenanikolicc@users.noreply.github.com>
2021-10-14 11:56:07 +0200

    Update build_all.sh

--- build_all.sh
+++ build_all.sh
@@ -6,4 +6,4 @@ docker build -t images.sbgenomics.com/nevena_nikolic/minimal-notebook-cuda:11.1 
 cd ../scipy-notebook
 docker build -t images.sbgenomics.com/nevena_nikolic/scipy-notebook-cuda:11.1 .
 cd ../tensorflow-notebook
-sudo docker build -t images.sbgenomics.com/nevena_nikolic/tensorflow-notebook-cuda:11.1.3.1.17 .
+sudo docker build -t images.sbgenomics.com/nevena_nikolic/tensorflow-notebook-cuda:11.1 .
