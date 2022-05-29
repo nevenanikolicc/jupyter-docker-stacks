780adc2982d6b13fecefcb60631b0856207189ae
Nevena Nikolic <nevenanikolic@Nevenas-MacBook-Pro.local>
2022-05-29 17:28:43 +0200

    update build_all.sh

--- build_all.sh
+++ build_all.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 cd base-notebook/
-docker build -t images.sbgenomics.com/nevena_nikolic/base-notebook-cuda:11.1 .
+docker build -t images.sbgenomics.com/nevena_nikolic/base-notebook-cuda:11.6 .
 cd ../minimal-notebook
-docker build -t images.sbgenomics.com/nevena_nikolic/minimal-notebook-cuda:11.1 .
+docker build -t images.sbgenomics.com/nevena_nikolic/minimal-notebook-cuda:11.6 .
 cd ../scipy-notebook
-docker build -t images.sbgenomics.com/nevena_nikolic/scipy-notebook-cuda:11.1 .
+docker build -t images.sbgenomics.com/nevena_nikolic/scipy-notebook-cuda:11.6 .
 cd ../tensorflow-notebook
-sudo docker build -t images.sbgenomics.com/nevena_nikolic/tensorflow-notebook-cuda:11.1.3.1.17 .
+sudo docker build -t images.sbgenomics.com/nevena_nikolic/tensorflow-notebook-cuda:11.6 .
